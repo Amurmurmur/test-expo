c1e54ca22945c69d6159c48ab5cf8dfdb47ad544
Amurmurmur <amurmurmur@gmail.com>
2023-07-04 13:28:30 +0100

    chore: reproducable demo

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Constants, { ExecutionEnvironment } from "expo-constants";
+import * as Sentry from "sentry-expo";
+import { useEffect } from "react";
+
+Sentry.init({
+  environment: Constants?.expoConfig?.extra?.sentryEnvironment,
+  dsn: Constants?.expoConfig?.extra?.sentryDsn,
+  enableInExpoDevelopment: true,
+  debug: true,
+});
 
 export default function App() {
+  useEffect(() => {
+    Promise.allSettled([]);
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
